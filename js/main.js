669ee7f2e1a0aef08a3937865377e24f8e3d2d08
SlawaSlawa <slawapocht4@yandex.ru>
2025-03-28 12:17:12 +0400

    update app

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Изменение шрифта
+
 const decreaseButton = document.getElementById('decreaseFont')
 const increaseButton = document.getElementById('increaseFont')
 
@@ -22,6 +24,8 @@ function increaseFontSize() {
 
 decreaseButton.addEventListener('click', decreaseFontSize)
 increaseButton.addEventListener('click', increaseFontSize)
+
+// Добавление ингредиента
 
 const recipe = []
 
@@ -60,4 +64,100 @@ document.querySelector('.add_btn').addEventListener('click', function () {
     document.querySelector('.recipe').append(div)
     name.value = ''
     count.value = ''
+})
+
+// Добавление названия рецепта
+
+document.querySelector('.recipe_name').addEventListener('click', function () {
+    let name = prompt('Введите название рецепта')
+
+    if (name) {
+        document.querySelector('.recipe_name').textContent = name
+    }
+})
+
+// Удаление ингредиента
+
+document.querySelector('.recipe').addEventListener('click', function (evt) {
+    const target = evt.target
+
+    if (!target.dataset.name) {
+        return false
+    }
+
+    target.closest('.d-flex').remove()
+    for (let i = 0; i < recipe.length; i++) {
+        if (recipe[i]['name'] === target.dataset.name) {
+            recipe.splice(i, 1)
+        }
+    }
+})
+
+// Копирование рецепта в буфер обмена
+
+const copyBtn = document.getElementById('copy_btn')
+const resultnewRecipeEl = document.querySelector('.copy')
+
+copyBtn.addEventListener('click', function () {
+    const copyText = resultnewRecipeEl.innerText
+
+    navigator.clipboard
+        .writeText(copyText)
+        .then(() => {
+            alert('Рецепт успешно скопирован!')
+        })
+        .catch((error) => {
+            alert('Что-то пошло не так, попробуйте позже!', error)
+        })
+})
+
+// Кнопка вычислить
+
+document.querySelector('.result_btn').addEventListener('click', function () {
+    document.querySelector('.result_new_recipe').textContent = ''
+    let ratio_type = +document.querySelector('.item_ratio_type').value
+    let ratio = +document.querySelector('.item_ratio').value
+
+    if (!ratio) {
+        alert('Введите число кроме нуля')
+        return false
+    }
+
+    const new_recipe = []
+
+    if (ratio_type === 1) {
+        for (let i = 0; i < recipe.length; i++) {
+            new_recipe.push({
+                name: recipe[i]['name'],
+                count: (+recipe[i]['count'] / ratio).toFixed(2),
+                type: recipe[i]['type'],
+            })
+        }
+    } else if (ratio_type === 2) {
+        for (let i = 0; i < recipe.length; i++) {
+            new_recipe.push({
+                name: recipe[i]['name'],
+                count: (+recipe[i]['count'] * ratio).toFixed(2),
+                type: recipe[i]['type'],
+            })
+        }
+    }
+
+    for (let i = 0; i < new_recipe.length; i++) {
+        let result
+
+        if (+new_recipe[i]['count'] === 0) {
+            result = 'по вкусу'
+        } else {
+            result = `${new_recipe[i]['count']} ${new_recipe[i]['type']}`
+        }
+
+        const div = document.createElement('div')
+        div.innerHTML = `
+            <div class="d-flex">
+                <div>${new_recipe[i]['name']} - ${result}</div>
+            </div>
+        `
+        document.querySelector('.result_new_recipe').append(div)
+    }
 })
